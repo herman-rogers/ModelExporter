567bb2929c7f70b928309489da9628076cc6fdad
Elysia <Elysia>
2016-05-31 15:53:51 +0100

    Add updated model combine script

--- Assets/Scripts/MeshCombine.cs
+++ Assets/Scripts/MeshCombine.cs
@@ -3,23 +3,36 @@ using System.Collections;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
-//[ExecuteInEditMode]
+[ExecuteInEditMode]
 public class MeshCombine : MonoBehaviour
 {
     void Start()
     {
+        Renderer cachedRenderer = null;
         MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
         CombineInstance[] combine = new CombineInstance[meshFilters.Length];
         int i = 1;
         while (i < meshFilters.Length)
         {
+            if (cachedRenderer == null)
+            {
+                cachedRenderer = meshFilters[i].GetComponent<Renderer>();
+            }
             combine[i].mesh = meshFilters[i].sharedMesh;
             combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
             meshFilters[i].gameObject.SetActive(false);
             i++;
         }
+        SetMaterialOnCombinedMesh(cachedRenderer);
+
         transform.GetComponent<MeshFilter>().mesh = new Mesh();
         transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
         transform.gameObject.SetActive(true);
+        DestroyImmediate(transform.gameObject.GetComponent<MeshCombine>());
+    }
+
+    private void SetMaterialOnCombinedMesh(Renderer cachedRenderer)
+    {
+        transform.GetComponent<Renderer>().material = cachedRenderer.material;
     }
 }
